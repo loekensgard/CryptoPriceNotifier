cd54745a79033c7ba860e9153a9a8d2341aa7054
Thorstein Løkensgard <loekensgard+github@gmail.com>
2024-12-21 13:52:06 +0100

    chore: change from btc to eth for default values

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
 
 export const getToken = () => process.env.DISCORD_TOKEN;
-export const getCryptoFiri = () => process.env.CRYPTO ?? "BTC";
-export const getCryptoGecko = () => process.env.CRYPTOGECKO ?? "bitcoin";
+export const getCryptoFiri = () => process.env.CRYPTO ?? "ETH";
+export const getCryptoGecko = () => process.env.CRYPTOGECKO ?? "ethereum";
